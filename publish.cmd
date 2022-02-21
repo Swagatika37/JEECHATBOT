ac13ab41522077ccc21483d251e4364281b61096
Swagatika37 <98446037+Swagatika37@users.noreply.github.com>
2022-02-21 20:44:16 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\covidbot123-bot-9502-Web-Deploy.pubxml -p:Password=X6rs7zx9PJDn4qFXskuct8a4rqSybgvhvvoeLsvTp2NjzeDeCofphSpZst9q
+
